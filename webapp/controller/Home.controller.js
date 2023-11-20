fc2ac455bb24b92d3917fd7e0d1336f50a234a7b
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-11-20 10:26:42 -0300

    feat: Message Toast

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,15 +1,31 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("com.lab2dev.firstapp.controller.Home", {
             onInit: function () {
 
+            },
+
+            onPress: function (oEvent) {
+
+                // Origem do evento
+                const item = oEvent.getSource();
+
+                // Título do item
+                const itemTitle = item.getTitle();
+
+                // Mensagem a ser exibida
+                const message = `O item: "${itemTitle}" foi clicado!`;
+
+                // Exibe uma mensagem na tela
+                MessageToast.show(message);
             }
         });
     });
