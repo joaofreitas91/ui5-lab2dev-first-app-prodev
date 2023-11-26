1e0a3fa8fbfa693bdf95b36aded065b1a3ebee59
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-11-26 13:37:12 -0300

    refactor: OData Model, Home controller and models

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,32 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/odata/v2/ODataModel",
+    "com/lab2dev/firstapp/model/models"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageBox, JSONModel, ODataModel) {
+    function (Controller, MessageBox, models) {
         "use strict";
 
         return Controller.extend("com.lab2dev.firstapp.controller.Home", {
             onInit: function () {
-                const oDataModel = new ODataModel("/northwind/northwind.svc/");
+                const products = models.getProducts(params);
 
-                oDataModel.read("/Products", {
-                    success: (oProducts) => {
-                        console.log(oProducts);
-                        const products = oProducts.results;
+                products.then((oProductsModel) => {
+                    this.getView().setModel(oProductsModel, 'products');
 
-                        console.log(products);
+                }).catch((oError) => {
+                    MessageBox.error(oError);
 
-                        const oModel = new JSONModel(products);
-                        this.getView().setModel(oModel, 'products');
-                    },
-                    error: (oError) => {
-                        MessageBox.error("Erro ao carregar os dados");
-                    }
                 });
             },
 
--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel",
     "sap/ui/Device"
-], 
+],
     /**
      * provide app-view type models (as in the first "V" in MVVC)
      * 
@@ -10,7 +11,7 @@ sap.ui.define([
      * 
      * @returns {Function} createDeviceModel() for providing runtime info for the device the UI5 app is running on
      */
-    function (JSONModel, Device) {
+    function (JSONModel, ODataModel, Device) {
         "use strict";
 
         return {
@@ -18,6 +19,41 @@ sap.ui.define([
                 var oModel = new JSONModel(Device);
                 oModel.setDefaultBindingMode("OneWay");
                 return oModel;
-        }
-    };
-});
+            },
+
+            getODataModel: function () {
+                var oDataModel = new ODataModel("/northwind/northwind.svc/");
+
+                return new Promise(function (resolve, reject) {
+                    oDataModel.attachMetadataLoaded(() => {
+                        resolve(oDataModel);
+                    });
+                    oDataModel.attachMetadataFailed(() => {
+                        reject("Serviço indisponível no momento.");
+                    });
+                });
+            },
+
+            getProducts: function () {
+                var oDataModel = this.getODataModel();
+
+                return new Promise((resolve, reject) => {
+                    oDataModel
+                        .then((oModel) => {
+                            oModel.read("/Products", {
+                                success: (oData) => {
+                                    resolve(new JSONModel(oData.results));
+                                },
+                                error: (oError) => {
+                                    reject(oError);
+                                }
+                            });
+                        })
+                        .catch((oError) => {
+                            reject(oError);
+                        });
+                });
+
+            }
+        };
+    });
