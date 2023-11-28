7f9615b5253b25c6d27dd68ba62cf1ceb3eb1d1e
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-11-28 20:29:18 -0300

    feat: formatter

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -3,15 +3,18 @@ sap.ui.define([
     "sap/m/MessageBox",
     "com/lab2dev/firstapp/model/models",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
+    "sap/ui/model/FilterOperator",
+    "com/lab2dev/firstapp/model/formatter"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageBox, models, Filter, FilterOperator) {
+    function (Controller, MessageBox, models, Filter, FilterOperator, formatter) {
         "use strict";
 
         return Controller.extend("com.lab2dev.firstapp.controller.Home", {
+            formatter: formatter,
+
             onInit: function () {
                 const params = {
                     urlParameters: {
