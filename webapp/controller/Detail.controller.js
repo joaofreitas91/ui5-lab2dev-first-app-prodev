9f6b8259dec8c39806f2abd2e348807d79d4aeea
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-11-30 20:02:23 -0300

    feat: nav back

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,14 +1,30 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, History) {
         "use strict";
 
         return Controller.extend("com.lab2dev.firstapp.controller.Detail", {
             onInit: function () {
+                console.log("Detail");
             },
+
+            onNavBack: function () {
+                const oHistory = History.getInstance();
+                const sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+
+                } else {
+                    const oComponent = this.getOwnerComponent()
+                    const oRouter = oComponent.getRouter();
+                    oRouter.navTo("RouteHome", {}, true);
+                }
+            }
         });
     });
