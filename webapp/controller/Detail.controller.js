a92ff29511020667df893948582dab86e6d273ca
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-11-30 22:32:41 -0300

    feat: get route params

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,16 +1,36 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
+    "sap/ui/core/routing/History",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, History) {
+    function (Controller, History, JSONModel) {
         "use strict";
 
         return Controller.extend("com.lab2dev.firstapp.controller.Detail", {
             onInit: function () {
-                console.log("Detail");
+                const oRouter = this.getOwnerComponent().getRouter();
+
+                //O método getRoute recebe como argumento o nome da rota que deseja adicionar o evento
+                const routeDetail = oRouter.getRoute("RouteDetail")
+
+                //O método attachPatternMatched recebe como argumento o nome da função que 
+                //será executada quando a rota for acessada
+                routeDetail.attachPatternMatched(this.onObjectMatched, this);
+            },
+
+            onObjectMatched: function (oEvent) {
+                //O método getParameter("arguments") retorna um objeto com os parâmetros passados na rota
+                const oArgs = oEvent.getParameter("arguments");
+                const productId = oArgs.productId;
+
+                const oProductModel = new JSONModel({
+                    productId: productId
+                });
+
+                this.getView().setModel(oProductModel, "detailView");
             },
 
             onNavBack: function () {
